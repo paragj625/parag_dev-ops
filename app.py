3dbe01112d3f0cbff1b9b00378304e078b169807
paragj625 <77636437+paragj625@users.noreply.github.com>
2025-11-03 16:19:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,38 +1,12 @@
-from flask import Flask, request, jsonify
+from flask import Flask
 import os
 
 app = Flask(__name__)
 
+@app.route("/")
+def hello():
+    return "Flask inside Docker!!!"
 
-
-@app.route("/", methods=["GET", "POST"])
-def calculate():
-    
-        # Get parameters from URL
-        num1 = float(request.args.get('num1'))
-        num2 = float(request.args.get('num2'))
-        operation = request.args.get('operation')
-
-        # Perform calculation
-        if operation == 'add':
-            result = num1 + num2
-        elif operation == 'sub':
-            result = num1 - num2
-        elif operation == 'mul':
-            result = num1 * num2
-        elif operation == 'div':
-            if num2 == 0:
-                return jsonify({"error": "Cannot divide by zero"}), 400
-            result = num1 / num2
-        else:
-            return jsonify({"error": "Invalid operation"}), 400
-
-        return jsonify({
-            "num1": num1,
-            "num2": num2,
-            "operation": operation,
-            "result": result
-        })
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
